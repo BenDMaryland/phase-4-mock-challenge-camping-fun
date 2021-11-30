58b00173bb54ef758d2b4cec51960e2341d699cd
My Name <ben.d.maryland@gmail.com>
2021-11-30 15:30:14 -0500

    commit tree 07e6ff090d7b202d9b26b681cca13c20d48d7e72

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,28 @@
+class CampersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    campers = Camper.all
+    render json: campers
+  end
+
+  def show
+    camper = Camper.find(params[:id])
+    render json: camper, serializer: CamperActivitySerializer
+  end
+
+  def create
+    camper = Camper.create!(camper_params)
+    render json: camper, status: 201
+  end
+
+  private
+
+  def render_not_found_response(notFound)
+    render json: { error: "Camper not found" }, status: :not_found
+  end
+
+  def camper_params
+    params.permit(:name, :age)
+  end
+end
